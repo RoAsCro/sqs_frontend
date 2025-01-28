a2c89017f9897b265937a655a579a92d4d51d336
Roland <rollycrompton@gmail.com>
2025-01-28 16:08:07 +0000

    Configured mid_priority queue

--- api.py
+++ api.py
@@ -5,13 +5,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 high_priority = getenv("HIGH_PRIORITY_QUEUE")
+mid_priority = getenv("MID_PRIORITY_QUEUE")
 access_id = getenv("ACCESS_ID")
 access_key = getenv("ACCESS_KEY")
 
 sqs = boto3.client("sqs",
                    region_name='us-east-1',
                    aws_access_key_id=access_id,
-                   aws_secret_access_key=access_key)
+                   aws_secret_access_key=access_key
+                   )
 
 
 router = Blueprint("messages", __name__, url_prefix="/")
@@ -24,28 +26,17 @@ def post_message():
         match message["priority"]:
             case "high":
                 queue_url = high_priority
+            case "mid":
+                queue_url = mid_priority
             case _:
                 raise ValueError("Not yet implemented")
 
         sqs.send_message(QueueUrl=queue_url,
                          DelaySeconds=30,
-                         MessageBody=f"Priority: {message['priority']}"
+                         MessageBody=f"Priority: {message['priority']}\n"
                                      f"{message['title']}: {message['message']}"
                          )
         return "message sent", 200
     else:
         return "failed to send", 500
-        # test_message()
-
-
-def test_message():
-    message = sqs.receive_message(QueueUrl=high_priority,
-    MaxNumberOfMessages=1,
-    VisibilityTimeout=0,
-    WaitTimeSeconds=0)
-    print(message['Messages'][0])
-    sqs.delete_message(
-        QueueUrl=high_priority,
-        ReceiptHandle=message['Messages'][0]['ReceiptHandle']
-    )
 
